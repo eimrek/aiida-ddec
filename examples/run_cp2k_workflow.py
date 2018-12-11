72433dba9967becdf3450ce3ce9a75de0744d675
eimrek <kristjaneimre@gmail.com>
2018-12-11 19:41:48 +0100

    atomic dens dir from code extra

--- examples/run_cp2k_workflow.py
+++ examples/run_cp2k_workflow.py
@@ -29,8 +29,19 @@ ddec_options = {
     "max_wallclock_seconds": 1 * 60 * 60,
     "withmpi": False,
     }
+
 cp2k_code = test_and_get_code('cp2k_6.1_18464@daint-s746', expected_code_type='cp2k')
 ddec_code = test_and_get_code('chargemol_09_26_2017@daint-s746', expected_code_type='ddec')
+
+ddec_params = ParameterData(dict={
+    "net charge"                               : 0.0,
+    "charge type"                              : "DDEC6",
+    "periodicity along A, B, and C vectors"    : [True, True, True,],
+    "compute BOs"                              : False,
+    "atomic densities directory complete path" : ddec_code.get_extra("atomic_dens_dir"),
+    "input filename"                           : "valence_density",
+})
+
 submit(DdecCp2kChargesWorkChain,
         structure=structure,
         cp2k_code=cp2k_code,
@@ -38,4 +49,5 @@ submit(DdecCp2kChargesWorkChain,
 #        cp2k_parent_folder=load_node(5337),
         ddec_code=ddec_code,
         _ddec_options=ddec_options,
+        ddec_params=ddec_params,
         )
